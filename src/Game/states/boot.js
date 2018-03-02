9d336e2f24e92a49c33cc040a5805ef66d3a7f30
Christopher Fox <cfox1290@gmail.com>
2018-03-02 13:10:00 -0700

    halfway updated

--- src/Game/states/boot.js
+++ src/Game/states/boot.js
@@ -1,5 +1,11 @@
 export default function bootState() {
     return {
+        preload: function(){
+            this.load.image('preloaderBackground', 'assets/preloadbck.png');
+            this.load.image('preloaderBar', 'assets/preloadbar.png');
+            //load all game text from text file to make localization in different games easy
+            this.load.text('textList','assets/BasicGame.txt');
+        },
         create: function () {
             this.game.physics.startSystem(window.Phaser.Physics.ARCADE);
             this.game.state.start('load');
